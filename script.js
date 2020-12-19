a903429c4c7886639ef1e9c6a863a3d7cecf85d6
Amanda <awiehrs.photo@gmail.com>
2020-12-18 23:51:18 -0500

    Formatted rows and tables, added styling to save button, in process of figuring out how to change from military time disaply to standard

--- script.js
+++ script.js
@@ -4,14 +4,33 @@
     console.log(todayDisplay)
     $("#currentDay").text(todayDisplay);
 
-    var currentTime = parseInt(moment().format('HH'));
+    var currentTime = parseInt(moment().format('hh'));
     console.log(currentTime)
 
-    var hour = [9,10,11,12,13,14,15,16,17]
+    var timeSlot = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : {}
 
-    var taskForm = $(".input-group-text")
+    const date = new Date();
+    const hour = date.getHours();
 
+    for (var i = 9; i < 18; i++) {
+        var timeDiv = $("<div>");
+        timeDiv.text(`${i}:00`);
+        timeDiv.addClass("col-md-1 hour");
+        $("#timeTable").append(timeDiv);
+    
+        var inputDiv = $("<textarea>");
+        inputDiv.addClass(`col-md-10 input textarea ${i<hour? 'past' : i>hour? 'future': 'present'}`);
+        inputDiv.text(timeSlot[i]|| "");
+        inputDiv.attr("id", `todo${i}`);
+        $("#timeTable").append(inputDiv);
 
+    
+        var saveBtnDiv = $("<button>");
+        saveBtnDiv.addClass("col-md-1 saveBtn");
+        saveBtnDiv.text("save");
+        saveBtnDiv.attr("id", i);
+        $("#timeTable").append(saveBtnDiv);
+    }
 
 
  });
