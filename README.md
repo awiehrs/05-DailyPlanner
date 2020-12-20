56a880e6ef5d1775c6294cfed496d7fb15b3342a
Amanda <awiehrs.photo@gmail.com>
2020-12-20 14:15:28 -0500

    Updated location of Clear button, added styling to clear button, and updated README. Application is comlpete and functional

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # 05-DailyPlanner
+Homework - Week 5: Daily Planner
+
+## Description
+
+The assignment was to create a functional daily work planner using jQuery and reference the online library "Moment" to display the accurate time wherever the user of the application is located.
+
+When the application loads, the user will see a date stamp located in the header that displays the current time. Beneath the header is a table with individual rows for the average 8hr. workday. The user can type out their goals for each hour, and click the 'Save' button to keep their goal in that same hour block if they refresh the app. 
+
+Between the hours of 9am to 5pm, the colors of each row will change depending on whether the hour has past, is currently happening, or will happen in the future. The user will notice that past hours change to gray, with the input text turning a dimmer color, the present hour will have a red shade with bold font, and upcoming hours will be green and have standard weight font.
+
+Once the day is complete, the user can click the "Start New Day" button in the header to clear the table and start the process again for the next day.
+
+The styling inspiration was to create an application for managing the daily stresses of a work day, but give the appearance a calming color scheme to hopefully relax the user.
+
+### Application Link
+
+Deployed Link: [https://awiehrs.github.io/05-DailyPlanner/]
+GitHub Repo: [https://github.com/awiehrs/05-DailyPlanner]
+
+### Screenshot of Application in Browser
+
+![screenshots]( assets/Work-Day-Scheduler.gif "Application GIF")
+
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-     
+    // Implementing moment library to display the current accurate time wherever the user is
     var todayDisplay = moment().format('dddd[| ] MMMM Do[,] YYYY');
     console.log(todayDisplay)
     $("#currentDay").text(todayDisplay);
@@ -7,13 +7,16 @@ $(document).ready(function () {
     var currentTime = parseInt(moment().format('hh'));
     console.log(currentTime)
 
+    //Adds key value to local storage and establishes variable to use as text if any task has been previously entered
     var timeSlot = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : {}
     
+    //Establishes the hours array to display in the time blocks on each row
     const date = new Date();
     let hour = date.getHours();
     
-
+    //For loop to display each row dynamically
     for (var i = 9; i < 18; i++) {
+        //override of 24hr time display to 12hr and adding meridian's for clarity
         let j = i 
         let timeType = "am"
         if( j > 11 ){
@@ -22,19 +25,21 @@ $(document).ready(function () {
         if (j > 12){
             j = j-12
         }
+        //adds text and bootstrap column styling to where each hour is displayed
         var timeDiv = $("<div>");
         const displayedTime =`${j}:00 ${timeType}`
         timeDiv.text(displayedTime);
         timeDiv.addClass("col-md-1 hour");
         $("#timeTable").append(timeDiv);
-    
+
+        //adds text and bootstrap column styling to where user will input task, establishes what color the row needs to be based off the current time of day
         var inputDiv = $("<textarea>");
         inputDiv.addClass(`col-md-10 input textarea ${i<hour? 'past' : i>hour? 'future': 'present'}`);
         inputDiv.text(timeSlot[i]|| "");
         inputDiv.attr("id", `todo${i}`);
         $("#timeTable").append(inputDiv);
 
-    
+        //adds a save button to the end of the row and establishes bootstrap column styling
         var saveBtnDiv = $("<button>");
         saveBtnDiv.addClass("col-md-1 saveBtn");
         saveBtnDiv.text("save");
@@ -42,7 +47,7 @@ $(document).ready(function () {
         $("#timeTable").append(saveBtnDiv);
     }
 
-
+    //event listener for clicking on the save button, logs information to the local storage
     $(".saveBtn").on("click", function(){
         var id = $(this).attr("id");
         var value = $(this).prev().val()
@@ -51,7 +56,7 @@ $(document).ready(function () {
        console.log(timeSlot);
        console.log(value);
     });   
-    
+    //event listener for "Start New Day" button in header, clears display and local storage
     $(".clearBtn").on("click", function(){
         window.localStorage.clear();
         $(".textarea").val('')
